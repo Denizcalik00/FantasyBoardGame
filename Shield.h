19a217bf21f2ae90247f453a8882049c45f6ce49
Denizcalik00 <denizcalik00@gmail.com>
2025-11-17 16:45:13 +0000

    Implement Shield class with defense and attack attributes
    Added Shield class to manage defense and attack effects.

--- Shield.h
+++ Shield.h
@@ -1,4 +1,28 @@
 #ifndef SHIELD_H
 #define SHIELD_H
 
+
+
+
+#include "Item.h"
+
+// Shield increase defence, big shield decrease attack
+class Shield : public Item
+{
+private:
+    int defenceBoost;
+    int attackPenalty;
+
+public:
+    Shield(std::string n, int w, int defBoost, int atkPenalty)
+        : Item(n, w, SHIELD)
+    {
+        defenceBoost = defBoost;
+        attackPenalty = atkPenalty;
+    }
+
+    void applyEffect(Character &c);
+    void removeEffect(Character &c);
+};
+
 #endif // SHIELD_H
