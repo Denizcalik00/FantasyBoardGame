9bc470328068113c0877c300667ce41ae6a9649f
Denizcalik00 <denizcalik00@gmail.com>
2025-11-17 16:46:15 +0000

    Update Weapon.h

--- Weapon.h
+++ Weapon.h
@@ -1,4 +1,26 @@
 #ifndef WEAPON_H
 #define WEAPON_H
 
+
+
+
+
+#include "Item.h"
+
+// Weapon increase attack for character
+class Weapon : public Item
+{
+private:
+    int attackBoost;
+
+public:
+    Weapon(std::string n, int w, int boost)
+        : Item(n, w, WEAPON)
+    {
+        attackBoost = boost;
+    }
+
+    void applyEffect(Character &c);   // add attack
+    void removeEffect(Character &c);  // remove attack
+};
 #endif // WEAPON_H
