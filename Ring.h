38f91f4a8b875f02b97606f8d03d0d9d30041715
Denizcalik00 <denizcalik00@gmail.com>
2025-11-17 10:35:25 +0000

    Implement Ring class with health and strength boosts
    Added Ring class to manage health and strength boosts.

--- Ring.h
+++ Ring.h
@@ -2,3 +2,25 @@
 #define RING_H
 
 #endif // RING_H
+
+#include "Item.h"
+
+// Ring can increase health or strength.
+// Player can have many rings.
+class Ring : public Item
+{
+private:
+    int healthBoost;
+    int strengthBoost;
+
+public:
+    Ring(std::string n, int w, int hBoost, int sBoost)
+        : Item(n, w, RING)
+    {
+        healthBoost = hBoost;
+        strengthBoost = sBoost;
+    }
+
+    void applyEffect(Character &c);
+    void removeEffect(Character &c);
+};
